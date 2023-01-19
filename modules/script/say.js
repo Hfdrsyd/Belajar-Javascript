3d06dabaa2f9ce80a4ba5faf1b20c074f0c858bb
Muhammad Hafidh Rosyadi <mhafidhrosyadi@gmail.com>
2023-01-19 21:08:22 +0700

    update belajar

---
+++ modules/script/say.js
@@ -0,0 +1,7 @@
+export function sayHello(name) {//export dilakukan dengan menulis ecara explicit
+  console.info(`Hello ${name}`);
+}
+export function sayGoodBye(name) {
+  console.info(`Gudbay ${name}`);
+}
+const name = "eko";
